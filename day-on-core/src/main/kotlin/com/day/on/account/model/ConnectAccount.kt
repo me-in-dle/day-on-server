02e22142375c1de17700030bb1ffa4ccdf5445bc
gosekose <ksyn1611@naver.com>
2025-09-10 21:46:58 +0900

    Feat: day-on-server 소셜 로그인 인터페이스 작업(2)
    TODO: 구현체 작성

--- day-on-core/src/main/kotlin/com/day/on/account/model/ConnectAccount.kt
+++ day-on-core/src/main/kotlin/com/day/on/account/model/ConnectAccount.kt
@@ -1,6 +1,7 @@
 package com.day.on.account.model
 
 import com.day.on.account.type.ConnectType
+import com.day.on.account.type.System
 import java.time.LocalDateTime
 
 data class ConnectAccount(
@@ -9,8 +10,8 @@ data class ConnectAccount(
     val accountId: Long,
     val connectType: ConnectType,
     val isEmailVerified: Boolean,
-    val createdId: String,
-    val createdAt: LocalDateTime,
-    val updatedId: String,
-    val updatedAt: LocalDateTime,
+    val createdId: String = System.SYSTEM_ID.id,
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+    val updatedId: String = System.SYSTEM_ID.id,
+    val updatedAt: LocalDateTime = LocalDateTime.now(),
 )
