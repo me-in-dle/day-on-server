a53270aa50ef5d064e55f51d9e7f9113eb572371
leedabin2 <12193152@inha.edu>
2025-09-11 21:56:33 +0900

    fix : 기존 소켓 설정 수정

--- day-on-application/src/main/kotlin/com/day/on/websocket/service/WebSocketService.kt
+++ day-on-application/src/main/kotlin/com/day/on/websocket/service/WebSocketService.kt
@@ -1,32 +1,34 @@
 package com.day.on.websocket.service
 
-import com.day.on.websocket.model.UserSubscription
 import com.day.on.websocket.usecase.inbound.WebSocketUseCase
 import com.day.on.websocket.usecase.outbound.MessagePublishPort
-import com.day.on.websocket.usecase.outbound.SubscriptionPort
+import com.day.on.websocket.usecase.outbound.UserPresencePort
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
 class WebSocketService (
-    private val subscriptionPort: SubscriptionPort,
-    private val messagePublishPort : MessagePublishPort
+    private val presencePort: UserPresencePort,
+    private val messagePublishPort : MessagePublishPort,
+    @Value("\${app.server-id}") private val serverId: String
 ) : WebSocketUseCase {
 
-    override fun subscribeToTopic(userId: String, topic: String) {
-        subscriptionPort.addSubscription(UserSubscription(userId, topic))
+    override fun handleConnection(userId: String, sessionId: String) {
+        // 유저 - 서버 Redis에 매핑
+        presencePort.bind(userId, serverId, sessionId)
     }
 
-    override fun unsubscribeFromTopic(userId: String, topic: String) {
-        subscriptionPort.removeSubscription(UserSubscription(userId, topic))
+    override fun handleDisconnection(userId: String, sessionId : String) {
+        presencePort.unbind(userId, sessionId)
     }
 
-    override fun handleDisconnection(userId: String) {
-        subscriptionPort.removeAllUserSubscriptions(userId)
-    }
-
-    override fun broadcast(payload: String, topic: String) {
+    override fun broadcast(payload: String) {
         messagePublishPort.publishBroadcast(payload)
     }
 
-
+    override fun sendToUser(userId: String, body: String) {
+        val targetServerId = presencePort.findServerIdBy(userId) ?: return
+        val json = """{"targetUserId":"$userId","body":"$body"}"""
+        messagePublishPort.publishToServer(targetServerId, json)
+    }
 }
