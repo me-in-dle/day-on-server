af743ca622338caa455f0da0a690afccfc44369a
gosekose <ksyn1611@naver.com>
2025-09-14 20:04:59 +0900

    Fix: 토큰 응답 및 리다이렉트 처리, forwardUrl 추가

--- day-on-provider/day-on-provider-jwt/src/main/kotlin/com/day/on/adapter/AuthenticationJwtTokenAdapter.kt
+++ day-on-provider/day-on-provider-jwt/src/main/kotlin/com/day/on/adapter/AuthenticationJwtTokenAdapter.kt
@@ -5,6 +5,7 @@ import com.day.on.account.usecase.outbound.AuthenticationTokenPort
 import com.day.on.common.outbound.CachePort
 import com.day.on.config.JwtTokenBuilder
 import org.springframework.stereotype.Component
+import java.time.Instant
 import java.util.concurrent.TimeUnit
 
 @Component
@@ -15,11 +16,11 @@ class AuthenticationJwtTokenAdapter(
     override fun issueAll(accountId: Long): AuthenticationToken {
         jwtTokenBuilder.buildJwtToken(
             subject = accountId.toString(),
-            tokenTime = ACCESS_TOKEN_TTL,
+            tokenTime = Instant.now().plusSeconds(ACCESS_TOKEN_TTL).toEpochMilli(),
         ).let { accessToken ->
             jwtTokenBuilder.buildJwtToken(
                 subject = accountId.toString(),
-                tokenTime = REFRESH_TOKEN_TTL,
+                tokenTime = Instant.now().plusSeconds(REFRESH_TOKEN_TTL).toEpochMilli(),
             ).let { refreshToken ->
                 cachePort.put(
                     key = "$ACCESS_TOKEN_PREFIX:$accountId",
