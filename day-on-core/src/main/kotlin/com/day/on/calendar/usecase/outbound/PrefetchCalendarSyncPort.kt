2db8799c72bb060d81af6899f1f194fdcd5aaaa8
leedabin2 <12193152@inha.edu>
2025-10-01 01:23:04 +0900

    chore/#41 : 이름 변경 asyn -> prefetch

--- day-on-core/src/main/kotlin/com/day/on/calendar/usecase/outbound/PrefetchCalendarSyncPort.kt
+++ day-on-core/src/main/kotlin/com/day/on/calendar/usecase/outbound/PrefetchCalendarSyncPort.kt
@@ -3,7 +3,7 @@ package com.day.on.calendar.usecase.outbound
 import com.day.on.account.type.ConnectType
 import java.time.LocalDate
 
-interface AsyncCalendarSyncPort {
+interface PrefetchCalendarSyncPort {
     fun prefetchIfNeeded(
         accountId: Long,
         connectType: ConnectType,
